f30dfff29d9aae382cbe61e2f60f58741dfe81bc
Yashpal Ramakrishnaiah <yashpal.ramakrishnaiah1@monash.edu>
2023-11-24 16:58:37 +1100

    moved files + auto deploy

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 import logging
 
@@ -7,10 +8,13 @@ app = Flask(__name__)
 logging.basicConfig(filename='logs/flask.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    return('Welcome to EHR-ML-Web app!!')
+# @app.route('/', methods=['GET'])
+# def hello():
+#     return('Welcome to EHR-ML-Web app!!')
 
+@app.route('/')
+def index():
+    return render_template('index.html', pairs=[], the_title="Presidents Index")
 
 if __name__ == "__main__":
     app.run(debug=True)
